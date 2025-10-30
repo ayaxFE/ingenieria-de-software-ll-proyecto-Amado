59870cddd70f4ffd8c10146339ffc8736873d413
THEfoxde <thefoxde2005@gmail.com>
2025-10-30 13:33:37 -0300

    Gestor

--- Main.java
+++ Main.java
@@ -2,15 +2,7 @@ public class Main {
     public static void main(String[] args) {
        
                
-        System.out.print("mensaje d epreueba ");
-         System.out.print("mensaje d epreueba ");
-
-        System.out.print("mensaje d epreueba ");
- System.out.print("mensaje d epreueba ");
- System.out.print("mensaje d epreueba ");
- System.out.print("mensaje d epreueba ");
- System.out.print("mensaje d epreueba ");
- System.out.print("mensaje d epreueba ");
+         
  
     }
 

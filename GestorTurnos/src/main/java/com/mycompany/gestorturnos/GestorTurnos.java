e0d56fe69e93978393ef7a09000af08a7719544e
FacuDeRenzo <127690457+FacuDeRenzo@users.noreply.github.com>
2025-10-30 16:25:43 -0300

    Todo el programa

---
+++ GestorTurnos/src/main/java/com/mycompany/gestorturnos/GestorTurnos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestorturnos;
+
+/**
+ *
+ * @author ayaxFE
+ */
+public class GestorTurnos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
